0efc920b13f590d8b7ce7aea71db6855bfe5516c
topgun007-oss <83120036+topgun007-oss@users.noreply.github.com>
2024-01-28 11:09:20 +0000

    modifies the nav and created job pages.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask , render_template , jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db , load_job_from_db
 
 
 app = Flask(__name__)
@@ -14,6 +14,13 @@ def hello_world():
 def list_jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
+
+@app.route("/job/<id>")# it will create a dynamic route 
+def show_job(id): # this function will receive id as a paramter
+  job = load_job_from_db(id)
+  if not job:
+    return "Not Found",404
+  return render_template('jobpage.html',job=job)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
