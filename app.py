081a0ea5e263734ed9fe1942387c5a754f780f41
topgun007-oss <83120036+topgun007-oss@users.noreply.github.com>
2024-01-28 08:28:16 +0000

    added connection to db

--- app.py
+++ app.py
@@ -1,41 +1,19 @@
 from flask import Flask , render_template , jsonify
+from database import load_jobs_from_db
+
 
 app = Flask(__name__)
-
-JOB =[
-  {
-    'id':1,
-    'title':'Data Analyst',
-    'location':'Bengaluru, India',
-    'salary':'Rs. 10,00,000'
-  },
-  {
-    'id':2,
-    'title':'Data Scientist',
-    'location':'Delhi, India',
-    'salary':'Rs. 15,00,000'
-  },
-  {
-    'id':3,
-    'title':'Frontend Engineer',
-    'location':'Remote',
-    'salary':'Rs. 12,00,000'
-  },
-  {
-    'id':4,
-    'title':'Backend Engineer',
-    'location':'San Francisco, USA',
-    'salary':'$120,000'
-  }
-]
+   
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html',jobs=JOB)
+  jobs = load_jobs_from_db()
+  return render_template('home.html',jobs=jobs)
 
 @app.route("/api/job")
 def list_jobs():
-  return jsonify(JOB)
+  jobs = load_jobs_from_db()
+  return jsonify(jobs)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
---
+++ database.py
@@ -0,0 +1,24 @@
+
+
+from sqlalchemy import create_engine  , text
+import os
+
+db_connection_string = os.environ['DB_CONNECTION_STRING']
+
+engine = create_engine(db_connection_string, connect_args={
+        "ssl": {
+            "ssl_ca": "/etc/ssl/cert.pem"
+        }
+  
+})
+
+def load_jobs_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from job"))
+    job = []
+    for row in result.all():
+      job.append(row._asdict())
+    return job  
+
+
+  
